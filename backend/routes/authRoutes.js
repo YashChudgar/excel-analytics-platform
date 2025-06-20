2a441d38ea5b1a55f8f4297c500071525d7506f7
YashChudgar <yashchudgar2004@gmail.com>
2025-06-20 11:12:57 +0530

    auth route modified

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, getProfile, updateProfile } = require("../controllers/authController");
+const { register, login, getProfile, updateProfile, googleLogin, checkUserExists,  resetPasswordDirect } = require("../controllers/authController");
 const { auth } = require("../middlewares/auth");
 
 // Public routes
 router.post("/register", register);
 router.post("/login", login);
+
+router.post("/google", googleLogin);
+router.post('/check-user', checkUserExists);
+router.post('/reset-password-direct', resetPasswordDirect);
 
 // Authenticated user routes
 router.get("/profile", auth, getProfile);
